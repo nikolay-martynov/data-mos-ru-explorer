b03deb022ea5a91ef8dba2fc8c8c26c89bf7918e
nikolay-martynov <nickolay.martinov@googlemail.com>
2019-09-30 21:56:10 +0300

    Import Spring boot generated project

---
+++ src/main/groovy/com/github/nikolaymartynov/datamosruexplorer/DataMosRuExplorerApplication.groovy
@@ -0,0 +1,13 @@
+package com.github.nikolaymartynov.datamosruexplorer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DataMosRuExplorerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DataMosRuExplorerApplication, args)
+	}
+
+}
